c710f0e02db7372cd8c857daf6166b1eb80b8969
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2024-04-25 11:44:24 +0330

    Initial Commit

---
+++ ButtonSheet/ButtonSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonSheetApp.swift
+//  ButtonSheet
+//
+//  Created by Omid Heydarzadeh on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ButtonSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
